b4974b10cc55d17cffd577197b20af71ccd7fd76
ahwayakchih <ahwayakchih@gmail.com>
2020-04-29 21:21:42 +0200

    chore: bump Node.js to v14.1.0

--- alpine-setup-node.sh
+++ alpine-setup-node.sh
@@ -7,7 +7,7 @@
 NODE_VERSION=${NODE_VERSION:-"$1"}
 
 if [ "$NODE_VERSION" = "" ] ; then
-	NODE_VERSION="14.0.0"
+	NODE_VERSION="14.1.0"
 fi
 
 echo "Making sure node user and group exists ($NODE_UID:$NODE_GID)"
